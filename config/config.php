c2fa70acff67e2f87451d41ddd083ad4f6cfdae4
josephmarcel <josephmarcel@joseph>
2012-10-04 15:07:06 +0300

    first commit

--- config/config.php
+++ config/config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 /*
@@ -25,32 +24,3 @@ define('DATABASE', 'softbill');
 $conn = mysql_connect(HOST, USER, PASSWORD) or die(mysql_error());
 $db = mysql_select_db(DATABASE);
 ?>
-=======
-<?php
-
-/*
- * 2012 Flight
- *
- * NOTICE OF LICENSE
- *
- * This source file is protected by copyright
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file.
- *
- *  @author Robert Loondo <robbyl@ovi.com>
- *  @copyright  2012 Flight
- *  @version  Release: $Revision: 1.0.0 $
- */
-
-define('HOST', 'localhost');
-define('USER', 'root');
-define('PASSWORD', '');
-define('DATABASE', 'college');
-
-$conn = mysql_connect(HOST, USER, PASSWORD) or die(mysql_error());
-$db = mysql_select_db(DATABASE);
-
-?>
->>>>>>> branch 'master' of https://github.com/robbyl/college.git
