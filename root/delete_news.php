96ef793f73729231a2210e3d5d3cc72906a92fd2
robby <robby@Robby-pc>
2013-02-06 17:46:05 +0300

    update

--- root/delete_news.php
+++ root/delete_news.php
@@ -6,7 +6,26 @@ require '../config/config.php';
 if (isset($_GET['id']) && !empty($_GET['id'])) {
 
     $id = clean($_GET['id']);
+    
+    // Obtaining news attachment name according to the given id
+    $query_attachment = "SELECT nws_attachment
+                           FROM news
+                          WHERE nws_id = '$id'
+                          LIMIT 1";
 
+    $result_attachment = mysql_query($query_attachment) or die(mysql_error());
+
+    $attachment = mysql_fetch_array($result_attachment);
+
+    $attachment_name = $attachment['nws_attachment'];
+
+    // Obtaining news attachment path
+    $attachment_path = 'uploads/docs/' . $attachment_name;
+
+    // Deleting news attachment file.
+    unlink($attachment_path);
+
+    // Deleting news detail in the database.
     $query_news = "DELETE FROM news
                          WHERE nws_id = '$id'";
 
