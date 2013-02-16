6cb4465d7be226f9f4f29b8c56111c98afd2c121
robby <robby@Robby-pc>
2013-02-16 13:52:27 +0300

    update

--- library.php
+++ library.php
@@ -7,7 +7,7 @@ $current_file_name = 'academics.php';
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>NEWS</title>
+        <title>LIBRARY</title>
         <!-- Stylesheet -->
         <link href="css/style.css" rel="stylesheet" type="text/css" />
         <link rel="stylesheet" type="text/css" href="css/ddsmoothmenu.css" />
